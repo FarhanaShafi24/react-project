e3035b46e7613f5357444003acdf04ea8643b77f
FarhanaShafi24 <farhanashafi817@gmail.com>
2026-09-18 10:29:42 +0530

    Initial React Project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello Firends</h1>
+//       <p>Chai Peelo</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+/*import "./App.css";
+function App(){
+  const tarinerName="Nirmal";
+  const trainingDay=3;
+  return(
+    <main className="app">
+      <section className="welcome-card">
+      <p className="day-label">React Day {trainingDay}</p>
+      <h1 className="title">College Course Explorer </h1>
+      <p>Trainer:{tarinerName}</p>
+      <p>React Learning Project</p>
+      <p>Used React,JSX,componets,CSS</p>
+      </section>   
+    </main>       
+  );
+}
+export default App;*/
+
+import "./App.css";
+import Navbar from "./compenents/navbar";
+function App(){
+  return(
+    <div>
+      <Navbar />
+      <main id="home">
+        <h1>Campus Connect</h1>
+        <p>Welcome to college Events and Clubs</p>
+      </main>
+      
+    </div>
+  );
+}
+export default App;
